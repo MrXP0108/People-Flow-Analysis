fix: improve enhancement precision via foreground masking

demo.py
@@ -35,53 +35,6 @@ def show_camera_warning():
     img = np.array(img_pil)
     cv2.imshow('!!!', img)
 
-def automatic_brightness_and_contrast(image, clip_hist_percent=1):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    brightness = cv2.mean(gray)[0]
-    
-    # Calculate grayscale histogram
-    hist = cv2.calcHist([gray],[0],None,[256],[0,256])
-    hist_size = len(hist)
-    
-    # Calculate cumulative distribution from the histogram
-    accumulator = []
-    accumulator.append(float(hist[0]))
-    for index in range(1, hist_size):
-        accumulator.append(accumulator[index -1] + float(hist[index]))
-    
-    # Locate points to clip
-    maximum = accumulator[-1]
-    clip_hist_percent *= (maximum/100.0)
-    clip_hist_percent /= 2.0
-    
-    # Locate left cut
-    minimum_gray = 0
-    while accumulator[minimum_gray] < clip_hist_percent:
-        minimum_gray += 1
-    
-    # Locate right cut
-    maximum_gray = hist_size -1
-    while accumulator[maximum_gray] >= (maximum - clip_hist_percent):
-        maximum_gray -= 1
-    
-    # Calculate alpha and beta values
-    alpha = 255 / (maximum_gray - minimum_gray)
-    beta = -minimum_gray * alpha
-    
-    '''
-    # Calculate new histogram with desired range and show histogram 
-    new_hist = cv2.calcHist([gray],[0],None,[256],[minimum_gray,maximum_gray])
-    plt.plot(hist)
-    plt.plot(new_hist)
-    plt.xlim([0,256])
-    plt.show()
-    '''
-
-    auto_result = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
-    sharpen_filter = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
-    auto_result = cv2.filter2D(auto_result, -1, sharpen_filter)
-    return (auto_result, alpha, beta)
-
 def main(args):
 
     # video reader
@@ -89,7 +42,7 @@ def main(args):
     fps = cap.get(cv2.CAP_PROP_FPS)
 
     # image enhancement
-    enhancer = LowLightEnhancer(lol_v2_syn=True, perc=True, alpha=1.0)
+    enhancer = LowLightEnhancer()
     handler = TamperHandler()
 
     # person detection
@@ -102,8 +55,6 @@ def main(args):
     entrance_manager = EntranceManager(f'{args.source_folder}/{args.entrance_coords}')
     entrance_coords = entrance_manager.coords
     n_person = 0
-    in_pos_count = [0]*len(entrance_manager.coords)
-    out_pos_count = [0]*len(entrance_manager.coords)
 
     frame_id = 1
     while True:
@@ -111,17 +62,20 @@ def main(args):
         if not ret: break
         if handler.fixed_tamper_flag:
             show_camera_warning()
-            if args.show_tamper_handler: handler.show_result()
-
-        # brightness = cv2.mean(cv2.cvtColor(frame_img, cv2.COLOR_BGR2GRAY))[0]
-        # if args.enhance_lowlight and brightness < 80:
-        #     frame_img = enhancer.enhance(frame_img)
-        #     frame_img = cv2.convertScaleAbs(frame_img, alpha=1.5, beta=50)
-        frame_img, alpha, beta = automatic_brightness_and_contrast(frame_img)
-
-        handler.detect(frame_img.copy(), frame_id, args.tamp_thresh, visualized=args.show_tamper_handler)
-
+            # if args.tamp_test: handler.show_result()
         h, w = frame_img.shape[:2]
+        
+        frame_img = enhancer.enhance(frame_img, \
+            cv2.resize(handler.fgmask, (w, h)),
+            cv2.resize(handler.e_bg, (w, h)))
+
+        handler.detect(frame_img.copy(), frame_id, args.tamp_thresh, visualized=args.tamp_test)
+
+        if args.tamp_test:
+            if cv2.waitKey(10) & 0xFF == ord('q'): break
+            frame_id += 1
+            continue
+
         if frame_id == 1:
             if h >= 700 or w >= 700: entrance_manager.factor = 2.5
             if h <= 300: entrance_manager.factor = 0.5
@@ -137,9 +91,9 @@ def main(args):
         dets = detector.get_dets(frame_img,args.conf_thresh)
         invalid_in_pos, all_out_pos = track_manager.update(dets,frame_id)
         for in_pos in invalid_in_pos:
-            in_pos_count[in_pos] -= 1
+            entrance_manager.in_pos_count[in_pos] -= 1
         for out_pos in all_out_pos:
-            out_pos_count[out_pos] += 1
+            entrance_manager.out_pos_count[out_pos] += 1
 
         # 標示進出口
         if args.show_entrances:
@@ -164,9 +118,9 @@ def main(args):
                 cv2.putText(frame_img, f'id: {det.track_id}, from: {entrance}, to: {exit}', \
                     (int(det.bb_left), int(det.bb_top)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
                 if tracker.update_pos(det.get_coord(), entrance_coords):
-                    in_pos_count[tracker.in_pos] += 1
+                    entrance_manager.in_pos_count[tracker.in_pos] += 1
 
-        show_updated_analysis(n_person, in_pos_count, out_pos_count)        
+        show_updated_analysis(n_person, entrance_manager.in_pos_count, entrance_manager.out_pos_count)        
         cv2.imshow('People Flow Analysis', frame_img)
         if cv2.waitKey(50) & 0xFF == ord('q'): break
 
@@ -190,10 +144,9 @@ parser.add_argument('--a', type=float, default=100.0, help='assignment threshold
 parser.add_argument('--cdt', type=int, default=5, help='coasted deletion time')
 parser.add_argument('--high_score', type=float, default=0.3, help='high score threshold')
 parser.add_argument('--conf_thresh', type=float, default=0.01, help='detection confidence threshold')
-parser.add_argument('--tamp_thresh', type=int, default=30, help='tampering confidence threshold')
+parser.add_argument('--tamp_thresh', type=int, default=50, help='tampering confidence threshold')
 parser.add_argument('--show_entrances', action='store_true', help='show the bounding boxes of entrances or not')
-parser.add_argument('--show_tamper_handler', action='store_true', help='visualize the process of tamper handling')
-parser.add_argument('--enhance_lowlight', action='store_true', help='enhance the frame image in low-light')
+parser.add_argument('--tamp_test', action='store_true', help='debug mode for tamper handling')
 args = parser.parse_args()
 
 if __name__ == '__main__':

enhancement/lowlight.py
@@ -1,80 +1,51 @@
-from torchvision import transforms
-from enhancement.loss.losses import *
-from enhancement.net.CIDNet import CIDNet
-from PIL import Image
 import cv2
 import numpy as np
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+import matplotlib.pyplot as plt
 
 class LowLightEnhancer:
-    def __init__(self, perc=False, lol=False, lol_v2_real=False, lol_v2_syn=False, \
-                 best_GT_mean=False, best_PSNR=False, best_SSIM=False, alpha=1.0):
-        self.perc = perc
-        self.lol = lol
-        self.lol_v2_real = lol_v2_real
-        self.lol_v2_syn = lol_v2_syn
-        self.best_GT_mean = best_GT_mean
-        self.best_PSNR = best_PSNR
-        self.best_SSIM = best_SSIM
-        self.alpha = alpha
-        """
-        lol + perc (weights that trained with perceptual loss)
-        lol        (weights that trained without perceptual loss)
+    def enhance(self, image, fgmask, e_bg, clip_hist_percent=1):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        
+        # Calculate grayscale histogram
+        mask = np.logical_and(fgmask != 255, e_bg != 255)
+        hist = cv2.calcHist([gray[mask]], [0], None, [256], [0, 256])
+        hist_size = len(hist)
+        
+        # Calculate cumulative distribution from the histogram
+        accumulator = []
+        accumulator.append(float(hist[0]))
+        for index in range(1, hist_size):
+            accumulator.append(accumulator[index -1] + float(hist[index]))
+        
+        # Locate points to clip
+        maximum = accumulator[-1]
+        clip_hist_percent *= (maximum/100.0)
+        clip_hist_percent /= 2.0
+        
+        # Locate left cut
+        minimum_gray = 0
+        while accumulator[minimum_gray] < clip_hist_percent:
+            minimum_gray += 1
+        
+        # Locate right cut
+        maximum_gray = hist_size -1
+        while accumulator[maximum_gray] >= (maximum - clip_hist_percent):
+            maximum_gray -= 1
+        
+        # Calculate alpha and beta values
+        alpha = 255 / (maximum_gray - minimum_gray)
+        beta = -minimum_gray * alpha
+        
+        '''
+        # Calculate new histogram with desired range and show histogram 
+        new_hist = cv2.calcHist([gray],[0],None,[256],[minimum_gray,maximum_gray])
+        plt.plot(hist)
+        plt.plot(new_hist)
+        plt.xlim([0,256])
+        plt.show()
+        '''
 
-        lol_v2_real + best_GT_mean (or best_PSNR or best_SSIM)
-
-        lol_v2_syn + perc (weights that trained with perceptual loss)
-        lol_v2_syn        (weights that trained without perceptual loss)
-        """
-        if self.lol:
-            if self.perc:
-                weight_path = 'enhancement/weights/LOLv1/w_perc.pth'
-            else:
-                weight_path = 'enhancement/weights/LOLv1/wo_perc.pth'
-                
-        elif self.lol_v2_real:
-            if self.best_GT_mean:
-                weight_path = 'enhancement/weights/LOLv2_real/w_perc.pth'
-            elif self.best_PSNR:
-                weight_path = 'enhancement/weights/LOLv2_real/best_PSNR.pth'
-            elif self.best_SSIM:
-                weight_path = 'enhancement/weights/LOLv2_real/best_SSIM.pth'
-                
-        elif self.lol_v2_syn:
-            if self.perc:
-                weight_path = 'enhancement/weights/LOLv2_syn/w_perc.pth'
-            else:
-                weight_path = 'enhancement/weights/LOLv2_syn/wo_perc.pth'
-
-        self.eval_net = CIDNet()
-        self.eval_net.to(device)
-        self.eval_net.load_state_dict(torch.load(weight_path, map_location=lambda storage, loc: storage))
-        self.eval_net.eval()
-
-    def enhance(self, img):
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        eval_data = Image.fromarray(img)
-        eval_data = transforms.ToTensor()(eval_data).unsqueeze(0)
-        torch.set_grad_enabled(False)
-        if self.lol:
-            self.eval_net.trans.gated = True
-        else:
-            self.eval_net.trans.gated2 = True
-            self.eval_net.trans.alpha = self.alpha
-        with torch.no_grad():
-            input = eval_data.to(device)
-            output = self.eval_net(input)
-                
-        output = torch.clamp(output.to(device),0,1).to(device)
-        output_img = np.asarray(transforms.ToPILImage()(output.squeeze(0)))
-        output_img = cv2.cvtColor(output_img, cv2.COLOR_RGB2BGR)
-
-        torch.cuda.empty_cache()
-        if self.lol:
-            self.eval_net.trans.gated = False
-        else:
-            self.eval_net.trans.gated2 = False
-        torch.set_grad_enabled(True)
-
-        return output_img
+        auto_result = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+        auto_result = cv2.convertScaleAbs(auto_result, alpha=1.5, beta=50)
+        auto_result = cv2.GaussianBlur(auto_result, (25, 25), 0, 0)
+        return auto_result

enhancement/tampering.py
@@ -4,20 +4,24 @@ import matplotlib.pyplot as plt
 
 class TamperHandler:
     def __init__(self):
-        self.fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
+        self.fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
         self.fgbg.setNMixtures(4)
         self.fgbg.setVarInit(100)
 
         self.t_fix = 100
         self.t_set = 3
-        self.tamper_flag = 0         # f_{n}^{t}
-        self.tamper_flag_count = 0   # count++ if tamper_flag=1
+        self.tamper_flag = False
+        self.tamper_flag_count = 0   # count++ if tamper_flag is true
         self.tamper_validation_count = 0  # S
         self.fixed_tamper_flag = False    # set by hand, turn off by hand
         self.aedr = 0
         self.edr_sum = 0     # sum(edr) if no tamper attack
-        self.bg_temp = None  # to save temp background
         self.w = 200         # size of the tamper validation window (length)   (w > t_fix)
+
+        self.bg = np.zeros((240, 320))
+        self.bg_is_set = False
+        self.e_bg = np.zeros((240, 320))
+        self.fgmask = np.zeros((240, 320))
 
         self.result_is_shown = False
 
@@ -33,48 +37,48 @@ class TamperHandler:
             cv2.imshow('frame_rgb', frame)
             cv2.moveWindow('frame_rgb', 500, 50)
 
-        fgmask = self.fgbg.apply(frame, learningRate = 0.002)
-        fgmask = cv2.erode(fgmask, np.ones((1,1)))
-        fgmask = cv2.dilate(fgmask, np.ones((5, 5)))
+        self.fgmask = self.fgbg.apply(frame, learningRate = 0.002)
+        self.fgmask = cv2.erode(self.fgmask, np.ones((1,1)))
+        self.fgmask = cv2.dilate(self.fgmask, np.ones((5, 5)))
         
-        if self.tamper_flag == 1:
-            bg = self.bg_temp
-        else:
-            bg = self.fgbg.getBackgroundImage()
-            self.bg_temp = bg
+        if not self.bg_is_set:
+            self.bg = self.fgbg.getBackgroundImage()
 
         if visualized:
-            cv2.imshow('fgmask', fgmask)
-            cv2.moveWindow('fgmask', 900, 225)
-            cv2.imshow('bg', bg)
+            cv2.imshow('self.fgmask', self.fgmask)
+            cv2.moveWindow('self.fgmask', 900, 225)
+            cv2.imshow('bg', self.bg)
             cv2.moveWindow('bg', 100, 50)
 
         # serve for edr
-        background_gray = cv2.cvtColor(bg, cv2.COLOR_BGR2GRAY)
+        background_gray = cv2.cvtColor(self.bg, cv2.COLOR_BGR2GRAY)
         frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         if frame_id > 1:
-            mask_to_exclude = (fgmask == 255)
+            mask_to_exclude = (self.fgmask == 255)
 
-            e_bg = self.sobel_edge_detection(background_gray)
+            if not self.bg_is_set:
+                self.e_bg = self.sobel_edge_detection(background_gray)
+                self.bg_is_set = True
             e_c = self.sobel_edge_detection(frame_gray)
 
             if visualized:
-                cv2.imshow('bg_sobel', e_bg)
+                cv2.imshow('bg_sobel', self.e_bg)
                 cv2.moveWindow('bg_sobel', 100, 400)
                 cv2.imshow('c_sobel', e_c)
                 cv2.moveWindow('c_sobel', 500, 400)
             
             # th
-            th = self.calculate_th(e_bg)
+            th = self.calculate_th()
 
             # calculate edr
-            e_bg[mask_to_exclude] = 0
+            e_bg_temp = self.e_bg.copy()
+            e_bg_temp[mask_to_exclude] = 0
             e_c[mask_to_exclude] = 0
-            edr = 1 - (np.sum(e_bg & e_c)) / (np.sum(e_bg) + 1)     # +1 avoid 0/0
+            edr = 1 - (np.sum(e_bg_temp & e_c)) / (np.sum(e_bg_temp) + 1)     # +1 avoid 0/0
 
             # TODO: 改善遮擋偵測演算法
-            # contours, _ = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            # contours, _ = cv2.findContours(self.fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             # m = 0.0
             # for contour in contours:
             #     # 計算輪廓的面積
@@ -86,18 +90,18 @@ class TamperHandler:
             #         break
             
             # aedr
-            if self.tamper_flag == 0: # If any tamper attack occurs, the AEDR keeps the current value
+            if not self.tamper_flag: # If any tamper attack occurs, the AEDR keeps the current value
                 self.edr_sum += edr
-                self.aedr = np.sum((1 - self.tamper_flag) * self.edr_sum) / (frame_id - self.tamper_flag_count)
+                self.aedr = np.sum(self.edr_sum) / (frame_id - self.tamper_flag_count)
 
             if frame_id > self.t_fix:    # generating background before monitoring
                 # tamper validation count:
                 self.tamper_validation_count = self.calculate_tamper_validation_count(self.tamper_validation_count, self.w, edr, self.aedr, th)
 
                 # tamper flag setting:
-                self.tamper_flag = 1 if self.tamper_validation_count > self.t_set else 0
+                self.tamper_flag = self.tamper_validation_count > self.t_set
 
-                self.tamper_flag_count += self.tamper_flag
+                self.tamper_flag_count += (1 if self.tamper_flag else 0)
 
                 if self.tamper_validation_count > threshold:
                     self.fixed_tamper_flag = True
@@ -111,7 +115,7 @@ class TamperHandler:
 
     def show_result(self, force_show=False):
         if not (force_show and self.result_is_shown):
-            plt.figure(figsize=(16, 9))
+            plt.figure(figsize=(10, 5))
             plt.plot(self.aedr_list, 'r', label='AEDR')
             plt.plot(self.edr_list, 'b', linestyle = ':', label='EDR')
             plt.plot(self.aedr_th_list, 'g', linestyle = '-.', label='AEDR + TH')
@@ -122,10 +126,11 @@ class TamperHandler:
             plt.xlabel('Frame number')
             plt.ylabel('Edge disappearance ratio')
             plt.legend()
-            plt.show()
-            self.result_is_shown = True
 
-    def sobel_edge_detection(self, image_gray, blur_ksize=7, sobel_ksize=1, skipping_threshold=10):
+            self.result_is_shown = True
+            plt.show()
+
+    def sobel_edge_detection(self, image_gray, blur_ksize=5, sobel_ksize=1, skipping_threshold=10):
         """
 
         Input:
@@ -140,7 +145,6 @@ class TamperHandler:
             Edge matrix ∈ {0, 255}
         """
         img_blurred = cv2.GaussianBlur(image_gray, (blur_ksize, blur_ksize), 0)
-        # img_blurred = cv2.filter2D(img_blurred, -1, np.array([[-1, -1, -1],[-1,  9, -1],[-1, -1, -1]]))
         img_blurred = cv2.filter2D(img_blurred, -1, np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]]))
         
         # sobel algorthm use cv2.CV_64F
@@ -160,28 +164,28 @@ class TamperHandler:
         img_sobel[img_sobel >= skipping_threshold] = 255
         return np.array(img_sobel, dtype=np.uint8)
 
-    def calculate_th(self, e_bg):
+    def calculate_th(self):
         '''
 
         Input:
         ---
-            e_bg: #edge pixel in the n-th background frame
+            self.e_bg: #edge pixel in the n-th background frame
 
         Output:
         ---
             Adaptive threshold (All the values used in paper)
         '''
-        h, w = e_bg.shape[0], e_bg.shape[1]
-        e_bg = np.sum(e_bg) / 255
+        h, w = self.e_bg.shape[0], self.e_bg.shape[1]
+        sum = np.sum(self.e_bg) / 255
 
-        if e_bg == 0:
+        if sum == 0:
             return -1.0
-        elif e_bg / (w * h) < 0.026 * 2:
-            return 150 / e_bg
-        elif 0.026*2 <= e_bg / (w * h) < 0.046 * 2:
-            return 400 / e_bg
+        elif sum / (w * h) < 0.026 * 2:
+            return 150 / sum
+        elif 0.026*2 <= sum / (w * h) < 0.046 * 2:
+            return 400 / sum
         else:
-            return 1500 / e_bg
+            return 1500 / sum
 
     def calculate_tamper_validation_count(self, tamper_validation_count, w, edr, aedr, th):
         '''